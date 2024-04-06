a61df44764de826834e408fa08c62df11f046d50
Luís Henrique <luislhg@hotmail.com>
2024-04-06 01:46:32 -0300

    Initial commit

---
+++ CSharpLambda.Assignment/SoftwareUsage.cs
@@ -0,0 +1,25 @@
+﻿public class SoftwareUsage : ISoftwareUsage
+{
+    public bool MaiorDeIdade(Profile profile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Resumo(Profile profile)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface ISoftwareUsage
+{
+    string Resumo(Profile profile);
+    bool MaiorDeIdade(Profile profile);
+}
+
+public class Profile
+{
+    public int Idade { get; set; }
+    public string Nome { get; set; }
+    public string Endereço { get; set; }
+}
---
+++ CSharpLambda.AssignmentTests/SoftwareUsageTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests;
+
+[TestClass()]
+public class SoftwareUsageTests
+{
+    [TestMethod()]
+    [DataRow(int.MinValue, false)]
+    [DataRow(-1, false)]
+    [DataRow(0, false)]
+    [DataRow(1, false)]
+    [DataRow(17, false)]
+    [DataRow(18, true)]
+    [DataRow(int.MaxValue, true)]
+    public void MaiorDeIdadeTest(int idade, bool maiorDeIdade)
+    {
+        // Arrange.
+        var serviço = new SoftwareUsage();
+        var profile = new Profile { Nome = "Pedro", Idade = idade, Endereço = "Av Atlântica, 9911" };
+
+        // Act.
+        var resultado = serviço.MaiorDeIdade(profile);
+
+        // Assert.
+        Assert.AreEqual(maiorDeIdade, resultado, "Não foi possível.");
+    }
+
+    [TestMethod()]
+    [DataRow(int.MinValue, false)]
+    [DataRow(-1, false)]
+    [DataRow(0, false)]
+    [DataRow(1, false)]
+    [DataRow(17, false)]
+    [DataRow(18, true)]
+    [DataRow(int.MaxValue, true)]
+    public void ResumoTest(string nome, string endereço, int idade, string resumo)
+    {
+        // Arrange.
+        var serviço = new SoftwareUsage();
+        var profile = new Profile { Nome = nome, Idade = idade, Endereço = endereço };
+
+        // Act.
+        var resultado = serviço.Resumo(profile);
+
+        // Assert.
+        Assert.AreEqual(resumo, resultado, "Não foi possível.");
+    }
+}
